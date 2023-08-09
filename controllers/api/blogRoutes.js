5aa7178e9f0f1f2b29d0d05623776ffe497d46e6
wontonchris <127700194+wontonchris@users.noreply.github.com>
2023-08-09 11:39:35 -0500

    changing things

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -1,15 +1,18 @@
 const router = require('express').Router();
-const { Project } = require('../../models');
+const { TechBlog } = require('../../models');
 const withAuth = require('../../utils/auth');
+
+
+Project
 
 router.post('/', withAuth, async (req, res) => {
   try {
-    const newProject = await Project.create({
+    const newTechBlog = await TechBlog.create({
       ...req.body,
       user_id: req.session.user_id,
     });
 
-    res.status(200).json(newProject);
+    res.status(200).json(newTechBlog);
   } catch (err) {
     res.status(400).json(err);
   }
@@ -17,19 +20,19 @@ router.post('/', withAuth, async (req, res) => {
 
 router.delete('/:id', withAuth, async (req, res) => {
   try {
-    const projectData = await Project.destroy({
+    const TechBlogtData = await TechBlog.destroy({
       where: {
         id: req.params.id,
         user_id: req.session.user_id,
       },
     });
 
-    if (!projectData) {
+    if (!TechBlogData) {
       res.status(404).json({ message: 'No project found with this id!' });
       return;
     }
 
-    res.status(200).json(projectData);
+    res.status(200).json(TechBlogData);
   } catch (err) {
     res.status(500).json(err);
   }
